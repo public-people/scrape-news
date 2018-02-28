5fa343c6168f7765c652f51e36cc71410d74b648
JD Bothma <jbothma@gmail.com>
2018-02-28 09:00:13 +0200

    WIP saving to DB

---
+++ scrapenews/models.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+import pytz
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date, DateTime
+import sqlalchemy.sql.functions as func
+
+
+Base = declarative_base()
+
+
+class Article(Base):
+    """
+    Data about a gazette that was scraped from the web
+    """
+    __tablename__ = 'article'
+
+    id = Column(Integer, primary_key=True)
+    url = Column(String,
+                 unique=True,
+                 nullable=False)
+    publication_name = Column(String,
+                              unique=True,
+                              nullable=False)
+    byline = Column(String,
+                    nullable=False)
+    publication_date = Column(Date,
+                              nullable=False)
+    created_at = Column(DateTime(timezone=True),
+                        nullable=False,
+                        server_default=func.now())
+    updated_at = Column(DateTime(timezone=True),
+                        nullable=False,
+                        server_default=func.now(),
+                        onupdate=func.current_timestamp())
+    body_html = Column(String,
+                       nullable=False,
+                       index=True)
+    title = Column(String,
+                   nullable=False,
+                   index=True)
+
+    def __repr__(self):
+        return "<Article(url=%r, title='%s')>" % (self.url, self.title)
