346ad5f10a81b6d59053e57e79a6db40c9731e91
JD Bothma <jbothma@gmail.com>
2018-02-28 12:35:59 +0200

    Store articles in db

--- scrapenews/pipelines.py
+++ scrapenews/pipelines.py
@@ -1,14 +1,45 @@
 # -*- coding: utf-8 -*-
 import dateutil.parser
 import logging
+from scrapenews.models import Article
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import os
 
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://sanews@localhost/sanews')
 logger = logging.getLogger(__name__)
 
 
 class ScrapenewsPipeline(object):
+    def __init__(self):
+        """
+        Initializes database connection and sessionmaker.
+        Creates deals table.
+        """
+        engine = create_engine(DATABASE_URL)
+        self.Session = sessionmaker(bind=engine)
+
     def process_item(self, item, spider):
 
         publication_date = dateutil.parser.parse(item['publication_date'])
 
-        logger.info("%r\n%r", publication_date, item)
+        session = self.Session()
+        article = Article(
+            url=item['url'],
+            publication_date=item['publication_date'],
+            publication_name=item['publication_name'],
+            byline=item['byline'],
+            title=item['title'],
+            body_html=item['body_html'],
+        )
+
+        try:
+            session.add(article)
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
         return item
