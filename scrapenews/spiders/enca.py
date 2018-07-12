ec68fcc97a4045aa1c9b349520c3900e08c528ed
Nicholas Spagnoletti <nicholas@nitric.co.za>
2018-07-12 20:46:27 +0200

    added eNCA / e-news channel africa to crawl all eNCA south african news

---
+++ scrapenews/spiders/enca.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapenews.items import ScrapenewsItem
+from datetime import datetime
+import re
+
+
+class ThenewageSpider(CrawlSpider):
+    name = 'enca'
+    allowed_domains = ['www.enca.com']
+    start_urls = ['http://www.enca.com/news-stream/south-africa']
+
+    link_extractor = LinkExtractor(allow=('/south-africa'))
+    rules = (
+        Rule(link_extractor, callback='parse_item', follow=True),
+    )
+
+    publication_name = 'eNCA'
+
+    def parse_item(self, response):
+        title = response.css('header.article-header h1').xpath('text()').extract_first()
+        self.logger.info('%s %s', response.url, title)
+        
+        publication_date = response.css('.article-meta time').xpath('@datetime').extract_first()
+        body_html = response.css('.article-text').extract_first()
+
+        if body_html:
+            item = ScrapenewsItem()
+            item['body_html'] = body_html
+            item['title'] = title
+            item['published_at'] = publication_date
+            item['retrieved_at'] = datetime.utcnow().isoformat()
+            item['url'] = response.url
+            item['file_name'] = response.url.split('/')[-1]
+            item['publication_name'] = self.publication_name
+            item['spider_name'] = self.name
+
+            yield item
+        else:
+            self.logger.info("No body found for %s", response.url)
