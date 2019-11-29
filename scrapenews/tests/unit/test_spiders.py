d1891d1ebcb847d3cc6a4bca6181782bfa726e79
Michael Currin <michael.currin@gmail.com>
2019-11-29 16:04:05 +0200

    test: Add spiders test

---
+++ scrapenews/tests/unit/test_spiders.py
@@ -0,0 +1,19 @@
+"""
+Test spiders.
+
+Light testing to ensure that the syntax of the spiders is correct by loading
+each spider class from the spider modules. Methods are not tested.
+"""
+from __future__ import absolute_import
+from unittest import TestCase
+
+
+from utils import list_spiders
+
+
+class TestSpiders(TestCase):
+
+    def test_names(self):
+        spiders = list_spiders.get_spiders()
+
+        self.assertTrue(len(spiders.keys()) > 0)
