4a0050f9b2fe328291cfe77e00eeae98619ade4f
Michael Currin <michael.currin@gmail.com>
2019-11-29 12:45:09 +0200

    style: Update comment

--- scrapenews/middlewares.py
+++ scrapenews/middlewares.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
+"""
+Define here the models for your spider middleware
 
-# Define here the models for your spider middleware
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-
+See documentation in:
+https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+"""
 from scrapy import signals
 
 
