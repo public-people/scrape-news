16424bc9114784d83dd56d5a8301debce4b7852f
Michael Currin <michael.currin@gmail.com>
2019-11-29 11:48:03 +0200

    feat: Add function

--- scrapenews/lib.py
+++ scrapenews/lib.py
@@ -5,6 +5,12 @@ def parse_date(value):
     return datetime.datetime.strptime(value[:10], '%Y-%m-%d')
 
 
-# import lib
 def parse_date_hour_min(value):
     return datetime.datetime.strptime(value[:16], '%Y-%m-%dT%H:%M')
+
+
+def parse_date_hour_min_sec(value):
+    """
+    Ignore parse datetime string down to seconds, ignoring microseconds.
+    """
+    return datetime.datetime.strptime(value[:19], '%Y-%m-%dT%H:%M:%S')
