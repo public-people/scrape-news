81a7a8b25d675fa3ed0892df4b16a572d97a5aa7
Michael Currin <michael.currin@gmail.com>
2019-12-01 10:56:35 +0200

    refactor: Remove PY2/3 line not needed in PY3

--- scrapenews/middlewares.py
+++ scrapenews/middlewares.py
@@ -4,7 +4,6 @@ Define here the models for your spider middleware
 See documentation in:
 https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 """
-from __future__ import absolute_import
 from scrapy import signals
 
 
