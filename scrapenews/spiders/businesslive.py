0e4e6a7f3433e99aa5a2620d14017c7b8210dcca
Michael Currin <michael.currin@gmail.com>
2019-12-01 16:00:44 +0200

    style: Update wrapping

--- scrapenews/spiders/businesslive.py
+++ scrapenews/spiders/businesslive.py
@@ -73,7 +73,9 @@ class BusinessLiveSpider(BusinessLiveMixin, SitemapSpider):
     sitemap_urls = ['https://www.businesslive.co.za/sitemap.xml']
     sitemap_follow = ['politics', 'companies', 'people', 'national', 'news',
                       'special-reports', 'economy', 'markets']
-    sitemap_rules = [('.*', 'parse_item')]
+    sitemap_rules = [
+        ('.*', 'parse_item')
+    ]
 
 
 class BusinessLiveCrawlSpider(BusinessLiveMixin, CrawlSpider):
